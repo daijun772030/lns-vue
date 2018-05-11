baa1b60830bef6c0723afa82f1994c50b31f9b83
daijun <904315004@qq.com>
2018-05-11 18:39:43 +0800

    提交

---
+++ src/pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+      这是一个回到底部的按钮
+  </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/Home',
+      name: 'home',
+      component: Home
+    }
+  ]
+})
