7228109e0ab2441a343d0e10fed982dfbec47444
daijun <904315004@qq.com>
2018-05-14 19:20:44 +0800

    初次提交

---
+++ src/components/home/home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <!-- <section>Hello World</section> -->
+    <div class="banner">
+      <div class="banner-text">
+        <!-- <p>小微企业贷</p>
+        <p>帮助企业资金周转</p> -->
+      </div>  
+    </div>
+    <div class="message">
+      <div class="lb">
+        <p>
+          爱鸿森借款APP上线啦！
+        </p>
+      </div>
+      <div class="">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+    <m-footer></m-footer>
+  </div>
+
+</template>
+
+<script>
+  import MFooter from 'base/m-footer/m-footer'
+
+  export default {
+    components: {
+      MFooter
+    }
+  }
+</script>
+
+<style ref="stylesheet" lang="stylus" scoped>
+  .banner
+    width 100%
+    height 410px
+    background-color blue
+  .message
+    width 100%
+    height 224px
+  .lb
+    width 90%
+    height 32px
+    background-color red
+    margin auto 5%
+
+</style>
+
